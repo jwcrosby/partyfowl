2ac823cdc129df045a9ba404468223ab304759a0
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-06 16:40:22 -0600

    build profile model

---
+++ models/profile.js
@@ -0,0 +1,31 @@
+import mongoose, { mongo } from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const profileSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    postalCode: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+    },
+    eventsAttending: { type: mongoose.Schema.Types.ObjectId, ref: 'Event'},
+    eventsSaved: { type: mongoose.Schema.Types.ObjectId, ref: 'Event'},
+    photos: { type: mongoose.Schema.Types.ObjectId, ref: 'Photo'},
+    friends: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile'}
+}, { timestamps: true})
+
+const Profile = mongoose.model('Profile', profileSchema)
+
+export {
+    Profile
+}
