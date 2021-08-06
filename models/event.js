2eb437c92783d9cb2b2c8f839884b8c17c469734
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-06 16:51:21 -0600

    build event model

---
+++ models/event.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const eventSchema = new Schema({
+    event_id: {
+        type: String,
+    },
+    description: {
+        type: String
+    },
+    title: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    start_date_time: {
+        type: Date
+    },
+    end_date_time: {
+        type: Date
+    },
+    event_photo: {
+        type: String
+    },
+    is_past: {
+        type: Boolean,
+        default: false
+    },
+    min_price: {
+        type: Number
+    },
+    max_price: {
+        type: Number
+    },
+    event_site: {
+        type: String
+    },
+    ticket_site: {
+        type: String
+    },
+    comments: [commentSchema],
+    user_photos: { type: mongoose.Schema.Types.ObjectId, ref: 'Photo'},
+    profiles_attending: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile'},
+}, { timestamps: true })
