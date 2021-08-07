6e62d33ec9ce5eff7a9a08db36fb561a81d01783
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-07 10:09:57 -0600

    stub events router

---
+++ routes/events.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import * as eventCtrl from '../controllers/events.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+// ========= Public Routes ===========
+
+
+
+// ========= Protected Routes ===========
+router.use(decodeUserFromToken)
+
+export {
+    router
+}
