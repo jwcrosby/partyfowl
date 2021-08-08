cdac8bf3dd971b9ad8cea50afb57e584fef84f50
jwcrosby <jwcrosby89@gmail.com>
2021-08-08 11:46:57 -0600

    add getEventsByPostalCode function

--- src/services/ticketmasterAPI.js
+++ src/services/ticketmasterAPI.js
@@ -1,18 +1,31 @@
 import React from "react";
 
-const getAllEvents = async () => {
+const getAllEvents = async (size) => {
   try {
-    let eventUrl = `https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=HwHRmjrv9cpx06oAoP2GNE1OkQ1CAej7`;
-    
+    let eventUrl = `https://app.ticketmaster.com/discovery/v2/events.json?size=${size}&apikey=HwHRmjrv9cpx06oAoP2GNE1OkQ1CAej7`;
+
     const res = await fetch(eventUrl);
-    
+
     const data = await res.json();
-    
+
     return data;
   } catch (error) {
     throw error;
   }
 };
 
-export { getAllEvents };
+const getEventsByPostalCode = async (size, postalCode) => {
+  try {
+    let eventUrl = `https://app.ticketmaster.com/discovery/v2/events.json?size=${size}&postalCode=${postalCode}&apikey=HwHRmjrv9cpx06oAoP2GNE1OkQ1CAej7`;
 
+    const res = await fetch(eventUrl);
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { getAllEvents, getEventsByPostalCode };
