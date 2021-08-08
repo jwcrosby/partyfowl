ed15d4c2a4f1a73085a369fbb7cfd8c4436ed105
jwcrosby <jwcrosby89@gmail.com>
2021-08-08 14:54:41 -0600

    code cleanup

--- src/components/Event/EventCard.jsx
+++ src/components/Event/EventCard.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './EventCard.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./EventCard.css";
 
 const EventCard = (props) => {
-  console.log(props)
+  console.log(props);
 
-    return (
-        <div className="event-card">
+  return (
+    <div className="event-card">
+      <div className="card-header">
+        <h3>{props.event.name}</h3>
+      </div>
 
-            <div className="card-header">
-              <h3>{props.event.name}</h3>
-            </div>
+      <div className="event-date">
+        <h5>{props.event.dates.start.localDate}</h5>
+      </div>
 
-            <div className="event-date">
-              <h5>{props.event.dates.start.localDate}</h5>
-            </div>
+      <div className="event-time">
+        <h6>{props.event.dates.start.localTime}</h6>
+      </div>
 
-            <div className="event-time">
-              <h6>{props.event.dates.start.localTime}</h6>
-            </div>
+      <div className="event-image">
+        <img className="img" src={props.event.images[3].url} alt="event"></img>
+      </div>
+    </div>
+  );
+};
 
-            <div className="event-image">
-            <img className="img" src={props.event.images[3].url} alt="event"></img>
-            </div>
-        </div>
-    )
-
-}
-
-export default EventCard
-
+export default EventCard;
