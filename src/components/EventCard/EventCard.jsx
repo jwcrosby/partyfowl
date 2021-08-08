ac5e90f1bb9a7b1202aa64b38dc8a6193d87f842
christinew2 <christinewei2@gmail.com>
2021-08-08 14:14:58 -0500

    make directory, jsx and css for eventcard component. stub up skeleton for jsx

---
+++ src/components/EventCard/EventCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styles from './EventCard.module.css'
+
+const EventCard = (props) => {
+    return (
+        <>
+            <h1>Event Card!</h1>
+        </>
+    )
+}
+
+export default EventCard
