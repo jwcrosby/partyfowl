179f56642688b50b91c8b3d499d570e482f55064
jwcrosby <jwcrosby89@gmail.com>
2021-08-08 13:10:30 -0600

    minor styling updates to landing.js

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "./Landing.module.css";
 import {
   getAllEvents,
@@ -20,7 +21,13 @@ const Landing = ({ user }) => {
           <h1>ALL EVENT DATA</h1>
           <div>
           {eventData?.map((event, index) => (
-                <p>{event.name}</p>
+            <Link to={{ pathname: "/events/" + event.id, state: event }}>
+            <div className="events" key={index}>
+              <div className="event-name">{event.name}</div>
+              <div className="event-type">{event.type}</div>
+              <img className={styles.img} src={event.images[3].url} alt="event"></img>
+            </div>
+            </Link>
             ))}
           </div>
         </div>
