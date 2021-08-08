87d0b9d34c9dcdce4143056bcd51d83c7656149c
jwcrosby <jwcrosby89@gmail.com>
2021-08-08 13:17:53 -0600

    update landing display

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -24,7 +24,8 @@ const Landing = ({ user }) => {
             <Link to={{ pathname: "/events/" + event.id, state: event }}>
             <div className="events" key={index}>
               <div className="event-name">{event.name}</div>
-              <div className="event-type">{event.type}</div>
+              <div className="event-date">{event.dates.start.localDate}</div>
+              <div className="event-time">{event.dates.start.localTime}</div>
               <img className={styles.img} src={event.images[3].url} alt="event"></img>
             </div>
             </Link>
