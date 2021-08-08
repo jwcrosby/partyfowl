0240d327733e092f00b980a34b8dda9122400f06
christinew2 <christinewei2@gmail.com>
2021-08-08 15:09:02 -0500

    add conditional in case no events exist

--- src/components/Event/EventList.jsx
+++ src/components/Event/EventList.jsx
@@ -7,18 +7,20 @@ import EventCard from './EventCard'
 
 
 const EventList = (props) => {
-// assuming/setting props as an array of event object ids?
+// assuming/setting props as an array of pre-populated event objects
 
     console.log('EVENT LIST!!')
 
     return (
-        props.map((event) => (
-            <EventCard
-                key={event._id}
-                event={event}
-                {...props}
-            />
-        ))
+        <div className="event-list">
+            {props?.map((event) => (
+                <EventCard
+                    key={event._id}
+                    event={event}
+                    {...props}
+                />
+            ))}
+        </div>
     )
 }
 
