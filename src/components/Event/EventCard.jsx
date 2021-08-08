bb50b6016609d86cb41c98b9a3cd09d1dd8e6301
christinew2 <christinewei2@gmail.com>
2021-08-08 15:38:18 -0500

    for merge

--- src/components/Event/EventCard.jsx
+++ src/components/Event/EventCard.jsx
@@ -3,10 +3,22 @@ import { NavLink } from 'react-router-dom'
 import styles from './Event.module.css'
 
 const EventCard = (props) => {
+// props should contain single event object containing all event data
+
+    console.log("THE PROPS IN EVENT CARD", props)
+
     return (
-        <>
-            <h1>Event Card!</h1>
-        </>
+        <div className="event-card">
+            <div className="card-header">
+                <h1>Insert Event Name</h1>
+            </div>
+            <div className="card-details">
+                <h1>Insert Event Name</h1>
+            </div>
+            <div className="card-header">
+                <h1>Insert Event Name</h1>
+            </div>
+        </div>
     )
 }
 
