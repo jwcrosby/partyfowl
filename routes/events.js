09a7cdc42b39d5005e51000fa80567dfb37cba5c
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-09 09:31:37 -0600

    define comment route

--- routes/events.js
+++ routes/events.js
@@ -10,6 +10,7 @@ const router = Router()
 
 // ========= Protected Routes ===========
 router.use(decodeUserFromToken)
+router.post('/:id', checkAuth, eventsCtrl.createComment)
 
 export {
     router
