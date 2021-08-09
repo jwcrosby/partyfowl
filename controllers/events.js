01c06c07411d569d50527bc4cfdac3b3d99ad461
christinew2 <christinewei2@gmail.com>
2021-08-09 12:05:07 -0500

    for merge

--- controllers/events.js
+++ controllers/events.js
@@ -3,6 +3,7 @@ import axios from "axios"
 // when you combine event.js controller files, be sure to also change routing
 
 function getAllEvents (req, res) {
+    console.log("I'M HITTING GETALLEVENTS")
     axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=100&sort=random&apikey=${process.env.API_KEY}`)
     .then(response => {
         res.json(response.data)
@@ -22,7 +23,6 @@ function getEventById (req,res){
         res.json(response.data)
     })  
 }
-
 
 export {
     getAllEvents,
