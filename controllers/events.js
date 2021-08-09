40339be1167ca956791b9d14bd3912bd83e5d7dc
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-09 09:39:04 -0600

    write createComment ctrl

--- controllers/events.js
+++ controllers/events.js
@@ -1,10 +1,20 @@
 import { User } from '../models/user.js'
 import { Event } from '../models/event.js'
 
-
+const createComment = async (req, res) => {
+    try {
+        const event = await Event.findById(req.params.id)
+        event.comments.push(req.body)
+        await event.save()
+        const newComment = event.comments[event.comments.length - 1]
+        return res.status(201).json(newComment)
+    } catch (err) {
+        res.status(400).send(err.message)
+    }
+}
 
 
 
 export {
-    
+    createComment
 }
