1938dc7700d0c54684006a56b58e33cd664cd60e
jwcrosby <jwcrosby89@gmail.com>
2021-08-09 15:20:43 -0600

    resize pin image

--- src/components/Event/EventDetailsMap.jsx
+++ src/components/Event/EventDetailsMap.jsx
@@ -1,25 +1,38 @@
 import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import { Link } from "react-router-dom";
+import ReactMapGL, { Marker } from 'react-map-gl'
+import pinImage from '../../assets/goose.jpg'
+import "./EventDetailsMap.css";
 
 // const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
-console.log(process.env)
 const EventDetailsMap = (props) => {
 
     const [viewport, setViewport] = useState({
       latitude: 37.8,
       longitude: -122.4, 
-      zoom: 3,
-      width: "30vw",
-      height: "30vh"
+      zoom: 16,
+      width: "40vw",
+      height: "40vh"
     });
+
+    console.log(props)
   
     return (
+      <>
         <ReactMapGL
         {...viewport}
         mapStyle="mapbox://styles/wcrosby/cks530lbr9l0s17t68dk9ozze"
         onViewportChange={setViewport}
         // mapboxApiAccessToken={TOKEN}
-      />
+        >
+        <div className="pin" key={"test"}>
+            <Link to={`/events/${"test"}`}>
+              <Marker latitude={parseFloat(37.8)} longitude={parseFloat(-122.4)} ><img className="pin" src={pinImage} alt="pin"></img></Marker>
+            </Link>
+        </div>
+
+        </ReactMapGL>
+      </>
     );
   }
 
