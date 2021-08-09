6c27853a25f2ac92d524bec6720a67ded702cc33
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-09 12:41:38 -0600

    add comment form to create

--- src/components/CreateComponents/CreateComment/CreateComment.jsx
+++ src/components/CreateComponents/CreateComment/CreateComment.jsx
@@ -3,7 +3,17 @@ import React, { useState } from 'react'
 const CreateComment = (props) => {
 
     return (
-        <form className='create-form'></form>
+        <form className='create-form'>
+            <label>Enter Your Comment Here</label>
+            <input 
+                required
+                type="text"
+                autoComplete='off'
+                placeholder='Comment'
+                name='comment-content'
+            ></input>
+            <button type='submit'>Submit</button>
+        </form>
     )
 }
 
