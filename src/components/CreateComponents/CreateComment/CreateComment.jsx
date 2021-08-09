36344de94fb995274ab8c04686907046bf1f7fa7
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-09 13:03:38 -0600

    write handle submit

--- src/components/CreateComponents/CreateComment/CreateComment.jsx
+++ src/components/CreateComponents/CreateComment/CreateComment.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react'
 
 const CreateComment = (props) => {
+    const [text, setText] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = {
+            comment_content: text,
+            event_id: props.event._id,
+            owner: props.currentUser._id,
+        }
+        props.handleCreateComment(formData)
+    }
 
     return (
-        <form className='create-form'>
+        <form onSubmit={handleSubmit} className='create-form'>
             <label>Enter Your Comment Here</label>
             <input 
                 required
-                type="text"
                 autoComplete='off'
                 placeholder='Comment'
-                name='comment-content'
+                name='comment_content'
+                value={text}
+                onChange={(e) => setText(e.target.value)}
             ></input>
             <button type='submit'>Submit</button>
         </form>
