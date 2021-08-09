6468bcd6bb475b3467b049d5d1c5fc350e406ea1
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-09 11:20:44 -0600

    stub create comment

---
+++ src/components/CreateComponents/CreateComment/CreateComment.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const CreateComment = (props) => {
+
+    return (
+        <form className='create-form'></form>
+    )
+}
+
+export default CreateComment
