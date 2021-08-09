a056e9a93f504f815e0ff78fc19b9c187f02822f
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-09 11:25:56 -0600

    stub comment card

---
+++ src/components/Comment/CommentCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import CommentActions from './CommentActions'
+
+
+const CommentCard = (props) => {
+
+    return (
+        <div className='comment-card'>
+            <div className='comment-content'></div>
+        </div>
+    )
+}
+
+export default CommentCard
