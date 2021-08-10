0dc7fd69ff01e42c080a313acf7023f63d265411
geedtd <grcazares@gmail.com>
2021-08-10 12:02:46 -0700

    margin, and styling on text for cards

--- src/components/Event/EventCard.jsx
+++ src/components/Event/EventCard.jsx
@@ -11,18 +11,19 @@ const EventCard = (props) => {
         <div className="event-image">
           <img className="img-event" src={props.event.images[3].url} alt="event"></img>
         </div>
+        <div className='card-text'>
       <div className="card-header">
-        <h3>{props.event.name}</h3>
+        <h3 className='card-details'>{props.event.name}</h3>
       </div>
 
         <div className="event-date">
-          <h5>{props.event.dates.start.localDate}</h5>
+          <h5 className='card-details'>{props.event.dates.start.localDate}</h5>
         </div> 
 
         <div className="event-time">
-          <h6>{props.event.dates.start.localTime}</h6>
+          <h6 className='card-details'>{props.event.dates.start.localTime}</h6>
         </div>
-
+        </div>
       </div>
       </Link>
     </>
