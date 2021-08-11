0cd4fd59de51514f77dd78187032465fd582b670
christinew2 <christinewei2@gmail.com>
2021-08-10 22:09:26 -0500

    create doesEventExist service and clean up dead code

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,25 +1,22 @@
 
-// const createEvent = async (id) => {
-//     try {
-//         console.log("I'm in the createEvent")
-//         let eventUrl = `/api/events/createEvent/${id}`;
-//         const res = await fetch(eventUrl);
-//         const data = await res.json();
-//         return data;
-//     } catch (error) {
-//         throw error;
-//     }
-// };
-
-// export { createEvent }
-
-
 export const createEvent = async (id) => {
     try {
         let EVENT_URL = '/api/events/createEvent'
         const res = await fetch(`${EVENT_URL}/${id}`, {
             method: "POST",
         }, { mode: "cors" })
+        const data = await res.json()
+        return data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const doesEventExist = async (id) => {
+    try {
+        let EVENT_URL = '/api/events/doesEventExist'
+        const res = await fetch(`${EVENT_URL}/${id}`)
+        // should return a boolean
         const data = await res.json()
         return data
     } catch (error) {
