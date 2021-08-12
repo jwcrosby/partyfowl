3a69abfd4cc5baf43ad64f186044954ca6d7c9ca
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-12 14:19:50 -0600

    stub create p cmmnt ctrl

---
+++ controllers/photos.js
@@ -0,0 +1,10 @@
+import { Photo } from '../models/photo.js'
+import { Event } from '../models/event.js'
+
+const createPhotoComment = async (req, res) => {
+
+}
+
+export {
+    createPhotoComment
+}
