71d3b2632d344bac7364088626e1d823d82cd7e0
jwcrosby <jwcrosby89@gmail.com>
2021-08-12 17:23:48 -0600

    remove cruft comments

--- src/components/Feed/SearchHeader.jsx
+++ src/components/Feed/SearchHeader.jsx
@@ -23,12 +23,6 @@ const SearchHeader = (props) => {
     try {
       setHasSearchRun(true);
 
-      // const data = await search(keyword);
-
-      // data.hasOwnProperty("_embedded")
-      //   ? setEventData(data._embedded.events)
-      //   : setEventData([]);
-
       history.push(`/events/search/${keyword}`);
       
     } catch (error) {
