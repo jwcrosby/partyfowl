15949776bf8745c089f55418547be9ef477ae771
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-12 17:53:34 -0600

    remove conditional to see if evt is in DB

--- src/pages/EventDetails/EventDetails.jsx
+++ src/pages/EventDetails/EventDetails.jsx
@@ -105,7 +105,7 @@ const EventDetails = (props) => {
     <div className='details-div'>
       
       <h1 className='details-h1'>{eventDetails.name}</h1>
-        {eventExists ? <h2>Event Exists - TESTING CONDITIONAL RENDERING</h2>: <h2>Event Doesn't Exist</h2>}
+
       <div className="display-img">
         {/* can refactor to make it a carousel */}
         {/* need to make conditional for if no images */}
