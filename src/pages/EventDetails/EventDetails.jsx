52c1101e80fc8e9e18af2cf6a2067b557fcee774
christinew2 <christinewei2@gmail.com>
2021-08-12 18:52:14 -0500

    remove dbEventsDetails state as it was not used

--- src/pages/EventDetails/EventDetails.jsx
+++ src/pages/EventDetails/EventDetails.jsx
@@ -19,7 +19,6 @@ const EventDetails = (props) => {
   const profile = props?.user?.profile
   const [eventExists, setEventExists] = useState(false)
   const [eventDetails, setEventDetails] = useState()
-  const [dbEventDetails, setDbEventDetails] = useState()
 
   const [commentsArray, setCommentsArray] = useState([])
   const [photoCommentsArray, setPhotoCommentsArray] = useState([])
@@ -50,7 +49,6 @@ const EventDetails = (props) => {
   const createEventOnClick = async() => {
     const res = await eventService.createEvent(id, eventDetails.name)
     setEventExists(true)
-    setDbEventDetails(res)
     setCommentsArray(res.comments)
     setPhotoCommentsArray(res.user_photos)
     setProfilesArray(res.profiles_attending)
@@ -69,23 +67,19 @@ const EventDetails = (props) => {
   useEffect(() => {
     const fetchEvent = async () => {
       try {
-        
         // if event exists, returns populated eventData; else, returns null
         const res = await eventService.doesEventExist(id)
         if (res) {
           setEventExists(true)
-          setDbEventDetails(res[0]) // may not really need this if we're saving info in separate states
           setCommentsArray(res[0].comments)
           setPhotoCommentsArray(res[0].user_photos)
-          setProfilesArray(res[0].profiles_attending)
-          
+          setProfilesArray(res[0].profiles_attending) 
         } else {
           setEventExists(false)
         }
 
         // returns ticketmaster event details
         const event = await ticketService.getEventById(id)
-        console.log(event)
         setEventDetails(event)
       } catch (error) {
         throw error
@@ -95,20 +89,16 @@ const EventDetails = (props) => {
   }, [id, eventExists])
 
   if (eventDetails === undefined) {
-    return <>Still loading...</>; // add a loading animation here
+    return <>Still loading...</>;
   }
   return (
-    
     <div className='details-div'>
       
       <h1 className='details-h1'>{eventDetails.name}</h1>
         {eventExists ? <h2>Event Exists - TESTING CONDITIONAL RENDERING</h2>: <h2>Event Doesn't Exist</h2>}
       <div className="display-img">
-        {/* can refactor to make it a carousel */}
-        {/* need to make conditional for if no images */}
         <img className='details-img' src={eventDetails.images[0].url} alt="event" />
       </div>
-
       <div className='map-n-details'>
         {/* <EventDetailsMap 
           eventDetails={eventDetails}
@@ -133,7 +123,6 @@ const EventDetails = (props) => {
                 ))}
               </div>
             }
-
           </div>
           <div className='links-container'>
             <a href={eventDetails?._embedded?.venues[0]?.url} rel='noreferrer' target='_blank'>
@@ -144,17 +133,13 @@ const EventDetails = (props) => {
             </a>
           </div>
 
-          
           <button className='comment-btn' onClick={() => handleAttendClick()}>
             I'm Attending This Event
-          </button>
-
-              
+          </button>   
         </div>
       </div>
 
       <div className="comments">
-
         {!eventExists && 
           <button className='comment-btn' onClick={() => createEventOnClick()}>
             Make The First Comment!
@@ -167,12 +152,10 @@ const EventDetails = (props) => {
             commentsArray={commentsArray}
             setCommentsArray={setCommentsArray}
           />
-        } 
-        
+        }     
       </div>
 
       <div className="photo-comments">
-
         {!eventExists && 
           <button className='photo-comment-btn' onClick={() => createEventOnClick()}>
             Add The First Photo!
@@ -185,8 +168,7 @@ const EventDetails = (props) => {
             photoCommentsArray={photoCommentsArray}
             setPhotoCommentsArray={setPhotoCommentsArray}
           />
-        } 
-        
+        }       
       </div>
     </div>
   );
